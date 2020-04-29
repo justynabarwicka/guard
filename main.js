44a4a258e7201a053559217fca46551c874c7cc7
Justyna Kowalska <justynakowalska@Justynas-MacBook-Pro.local>
2020-04-29 18:55:44 +0200

    zmiany JS

--- main.js
+++ main.js
@@ -9,13 +9,19 @@ angular.module("myApp", []).controller("MainController", ["$scope", "$timeout", 
         salary: {col:0, row:13, value: 0},
         food: {col:2, row:60, value: 0},
         home: {col:1, row:13, value: 0},
-        expensesSum: {col:6, row:1, value: 0}
+        other: {col:3, row:60, value:0},
+        fun: {col:4, row:60, value:0},
+        expensesSum: {col:6, row:1, value:0}
     };
 
     vm.formData = {
         wantSave: {address:"B16:B16", value: null},
         salary: {colId:0, colName:"A", address:"", value: null},
-        food: {colId:2, colName:"C", address:"", value: null}
+        food: {colId:2, colName:"C", address:"", value: null},
+        home: {colId:1, colName:"B", address:"", value: null},
+        other: {colId:3, colName:"D", address:"", value: null},
+        fun: {colId:4, colName:"E", address:"", value: null},
+        expensesSum: {colId:6, colName:"G", address:"", value: null}
     };
 
     const CLIENT_ID = "1065006613256-kq5d6m2ki006in7jgnatp1pr7b6fqejs.apps.googleusercontent.com",
